9b4e4c181f3e12f0265b6481e754962c6cfc1310
ReejoJoseph1244 <reejojoseph1244@gmail.com>
2023-06-07 20:24:42 +0530

    added new files

--- multiply.java
+++ multiply.java
@@ -2,6 +2,8 @@ public class multiply {
     public static void main(String[] args) {
         int a = 4;
         int b = 2;
+        System.out.println("Numbers are 4,2");
         System.out.println("The product is " + (a*b));
     }
+    
 }
